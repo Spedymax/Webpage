802a23ef7251fd8cac2249f8697fb0734b8229d9
Maksym Sokolov <mso@anthillagency.com>
2024-01-15 18:06:48 +0100

    lab4 final

--- lab4/first/accordion.js
+++ lab4/first/accordion.js
@@ -6,6 +6,7 @@ function createAccordion(count) {
         var accordionItem = document.createElement("div");
         accordionItem.classList.add("accordion-item");
         accordionItem.innerHTML = `
+            <input type="text" class="accordion-header-input" value="Розділ ${i + 1}" oninput="saveAccordionChanges()">
             <div class="accordion-header" onclick="toggleAccordion(${i})">Розділ ${i + 1}</div>
             <div class="accordion-content" id="content-${i}" style="display:none;">
                 <textarea oninput="saveAccordionChanges()">Введіть ваш текст тут...</textarea>
@@ -14,11 +15,13 @@ function createAccordion(count) {
         accordionContainer.appendChild(accordionItem);
     }
 }
+
 function saveAccordionChanges() {
     var accordionData = [];
     document.querySelectorAll('.accordion-item').forEach(function(item) {
+        var header = item.querySelector('.accordion-header-input').value;
         var content = item.querySelector('textarea').value;
-        accordionData.push(content);
+        accordionData.push({header: header, content: content});
     });
 
     var xhr = new XMLHttpRequest();
@@ -28,5 +31,6 @@ function saveAccordionChanges() {
 
     xhr.onload = function() {
         console.log("Зміни збережено: " + this.responseText);
+        alert('Збережено');
     };
 }
